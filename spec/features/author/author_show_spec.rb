7e92908cbe2384b2765c3cfe63ffbd32217e05bc
Janis Wehen <janiswehen@gmail.com>
2022-10-31 12:15:48 +0100

    failing test rework for issue #5

--- spec/features/author/author_show_spec.rb
+++ spec/features/author/author_show_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 describe "author show page", type: :feature do
-  context "given an author named Alan Turing" do
-    @alan = Author.new(
+  it "should display a Author Page for Alan Turing" do
+    expect{
+      @alan = Author.create(
         first_name: "Alan",
         last_name: "Turing",
-        homepage: "http://wikipedia.org/Alan_Turing",
-    )
-    @alan.save
-
-    it "should display a Author Page for Alan Turing" do
-        visit author_path(@alan)
-        expect(page).to have_text("First Name:")
-        expect(page).to have_text("Alan")
-        expect(page).to have_text("Last Name:")
-        expect(page).to have_text("Turing")
-        expect(page).to have_text("Homepage:")
-        expect(page).to have_text("http://wikipedia.org/Alan_Turing")
-    end
+        homepage: "http://wikipedia.org/Alan_Turing"
+      )
+    }.to change(Author, :count).by(1)
+    visit author_path(@alan)
+    expect(page).to have_text("First Name:")
+    expect(page).to have_text("Alan")
+    expect(page).to have_text("Last Name:")
+    expect(page).to have_text("Turing")
+    expect(page).to have_text("Homepage:")
+    expect(page).to have_text("http://wikipedia.org/Alan_Turing")
   end
 end
