807229d63e153d3779ded3ad99f562fe785f7521
Janis Wehen <janiswehen@gmail.com>
2022-11-01 21:12:25 +0100

    failing test for issue #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @paper = [
+        {paper: Paper.new(title: 'a' , venue: 'b', year: 1), isValid: true},
+        {paper: Paper.new(title: 'a' , venue: 'b'), isValid: false},
+        {paper: Paper.new(title: 'a' , venue: '', year: 1), isValid: false},
+        {paper: Paper.new(title: 'a' , venue: ''), isValid: false},
+        {paper: Paper.new(title: '' , venue: 'b', year: 1), isValid: false},
+        {paper: Paper.new(title: '' , venue: 'b'), isValid: false},
+        {paper: Paper.new(title: '' , venue: '', year: 1), isValid: false},
+        {paper: Paper.new(title: '' , venue: ''), isValid: false},
+        {paper: Paper.new(title: 'a' , venue: 'b', year: 'a'), isValid: false}
+    ]
+end
+
+it "should not validate if the last name is missing" do
+    @paper.each do |paper_obj|
+        expect(paper_obj[:paper].valid?).to eq(paper_obj[:isValid])
+    end
+end
 end
