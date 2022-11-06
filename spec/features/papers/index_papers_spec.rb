59f3af958fe175a623ccc5bfd32d2ca676db5994
Janis Wehen <janiswehen@gmail.com>
2022-11-06 17:42:49 +0100

    failing test for issue #19

--- spec/features/papers/index_papers_spec.rb
+++ spec/features/papers/index_papers_spec.rb
@@ -25,4 +25,27 @@ describe "index papers page", type: :feature do
             expect(page).to have_xpath("//a[@data-turbo-method='delete' and @href='#{paper_path(@paper)}']")
         end
     end
+
+    context "given a paper from 1959 and a Paper from 1968" do
+        before :each do
+            @paper1 = Paper.new(
+                title: 'Paper1',
+                venue: 'Content',
+                year: 1950
+            )
+            @paper1.save
+            @paper2 = Paper.new(
+                title: 'Paper2',
+                venue: 'Content',
+                year: 1968
+            )
+            @paper2.save
+        end
+
+        it "schould only show the paper1 when given a filter year=1950 via the url" do
+            visit papers_path + "?year=1950"
+            expect(page).to have_xpath("//a[@href='#{paper_path(@paper1)}']")
+            expect(page).not_to have_xpath("//a[@href='#{paper_path(@paper2)}']")
+        end
+    end
 end
